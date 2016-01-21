7f0c652a20738738dcb02aeba546ce77609b6b31
Tanay Chowdhury <tanayz@outlook.com>
2016-01-21 08:50:41 -0600

    Adding from blog

--- test1.py
+++ test1.py
@@ -1,11 +1,23 @@
 from kivy.app import App
-from kivy.uix.button import Button
+#from kivy.uix.button import Button
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 
 class TutorialApp(App):
     def build(self):
-        return Button()
+        f = FloatLayout()
+        s = Scatter()
+        l = Label(text='Hello!',
+                  background_color=(0, 0, 1, 1),
+                  font_size=150)
+        f.add_widget(s)
+        s.add_widget(l)
 
-if __name__=="main":
-    TutorialApp().run()
+        return f
+          
+#if __name__=="main":
+
+TutorialApp().run()
     
     

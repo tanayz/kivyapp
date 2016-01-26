6a58906c1778b7ead13682a517509e9578282206
Tanay Chowdhury <tanayz@outlook.com>
2016-01-26 02:00:00 -0600

    Testing FileChooserIconView

--- imgupd/main.py
+++ imgupd/main.py
@@ -1,62 +1,31 @@
+import kivy
+kivy.require('1.0.6')
+
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
 
-import os
+from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
 
 
-class LoadDialog(FloatLayout):
-    load = ObjectProperty(None)
-    cancel = ObjectProperty(None)
+#class Showcase(FloatLayout):
+#    pass
+#
+#
+#class KivyImageScatter(Scatter):
+#    pass
+#
+#
+#class ButtonsScatter(Scatter):
+#    pass
 
 
-class SaveDialog(FloatLayout):
-    save = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-    cancel = ObjectProperty(None)
+class ShowcaseApp(App):
+
+    def show_filechooser_icon(self):
+        return FileChooserIconView()
+
+    def show_filechooser_list(self):
+        return FileChooserListView()
 
 
-class Root(FloatLayout):
-    loadfile = ObjectProperty(None)
-    savefile = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-
-    def dismiss_popup(self):
-        self._popup.dismiss()
-
-    def show_load(self):
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    def show_save(self):
-        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Save file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    def load(self, path, filename):
-        with open(os.path.join(path, filename[0])) as stream:
-            self.text_input.text = stream.read()
-
-        self.dismiss_popup()
-
-    def save(self, path, filename):
-        with open(os.path.join(path, filename), 'w') as stream:
-            stream.write(self.text_input.text)
-
-        self.dismiss_popup()
-
-
-class Editor(App):
-    pass
-
-Factory.register('Root', cls=Root)
-Factory.register('LoadDialog', cls=LoadDialog)
-Factory.register('SaveDialog', cls=SaveDialog)
-
-if __name__ == '__main__':
-    Editor().run()
+if __name__ in ('__main__', '__android__'):
+    ShowcaseApp().run()
